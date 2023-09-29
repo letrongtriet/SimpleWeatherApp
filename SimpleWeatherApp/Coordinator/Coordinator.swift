71569edaae443bf2afb43eea8ceaec217c6c5eb8
Triet Le <letrongtriet@gmail.com>
2023-09-29 08:16:16 +0300

    Implement based protocol for Coordinator

---
+++ SimpleWeatherApp/Coordinator/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  SimpleWeatherApp
+//
+//  Created by Triet Le on 29.9.2023.
+//
+
+import Foundation
+
+// From here we can expand more functionalities, e.g, stop()
+protocol Coordinator: AnyObject {
+    func start()
+}
