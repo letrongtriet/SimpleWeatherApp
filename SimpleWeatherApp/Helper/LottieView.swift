415c8ca8556bbe0f94d75c1e006303bb1356bcff
Triet Le <letrongtriet@gmail.com>
2023-09-29 08:32:01 +0300

    Add a helper class to expose Lottie to SwiftUI

---
+++ SimpleWeatherApp/Helper/LottieView.swift
@@ -0,0 +1,39 @@
+//
+//  LottieView.swift
+//  SimpleWeatherApp
+//
+//  Created by Triet Le on 29.9.2023.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    let name: String
+    let loopMode: LottieLoopMode
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = LottieAnimationView()
+        
+        let animation = LottieAnimation.named(name)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
