1f63fb085b6a6a66ae76426fa346fa5ed2748e03
Triet Le <letrongtriet@gmail.com>
2023-10-01 07:55:08 +0300

    Remove AppDelegate and move to all SwiftUI

---
+++ SimpleWeatherApp/App/AppView.swift
@@ -0,0 +1,17 @@
+//
+//  AppView.swift
+//  SimpleWeatherApp
+//
+//  Created by Triet Le on 30.9.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppView: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
